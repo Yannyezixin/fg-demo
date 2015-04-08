a38fca1f5dc290303651fce7aa3bdfda32df29e0
Yannyezixin <646840080@qq.com>
2015-04-09 00:37:55 +0800

    修复 动画未完成便进行下一次滚动事件冲突

--- asset/js/pageover.js
+++ asset/js/pageover.js
@@ -2,13 +2,20 @@ $(document).ready(function () {
     slens = $('.section').length;
     curSide = 0;
     $('body').on('mousewheel', function (event, delta) {
-        $('.container').removeClass('s'+curSide);
-        if (delta < 0) {
-            curSide = (curSide + 1)%slens;
-        } else {
-            curSide = (curSide - 1 + slens)%slens;
+        if (!$('.animating')[0]) {
+            container = $('.container');
+            container.removeClass('s'+curSide);
+            if (delta < 0) {
+                curSide = (curSide + 1)%slens;
+            } else {
+                curSide = (curSide - 1 + slens)%slens;
+            }
+            child = container.children().eq(curSide);
+            child.addClass('animating');
+            container.addClass('s'+curSide);
+            container.one('transitionend webkitTransitionend', function () {
+                child.removeClass('animating');
+            });
         }
-        $('.container').addClass('s'+curSide);
-        console.log(curSide);
     });
 });
